bb801238baaeebddc9c2834d44b93c7ca80c8ee5
Samuelms <samuel.sarimo@gmail.com>
2026-04-22 14:49:42 +0300

    Add common math operations

--- mathmy.c
+++ mathmy.c
@@ -1,4 +1,5 @@
 #include "mathmy.h"
+#include <math.h>
 
 Vec2 mathmy_vec2_add(Vec2 a, Vec2 b) {
     Vec2 result;
@@ -6,6 +7,37 @@ Vec2 mathmy_vec2_add(Vec2 a, Vec2 b) {
     result.y = a.y + b.y;
     return result;
 }
+
+Vec2 mathmy_vec2_sub(Vec2 a, Vec2 b) {
+    Vec2 result;
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+    return result;
+}
+
+Vec2 mathmy_vec2_scale(Vec2 v, float scalar) {
+    Vec2 result;
+    result.x = v.x * scalar;
+    result.y = v.y * scalar;
+    return result;
+}
+
+float mathmy_vec2_squared_lenght(Vec2 v) {
+    return (v.x * v.x) + (v.y * v.y);
+}
+
+Vec2 mathmy_vec2_norm(Vec2 v) {
+    float length = sqrtf(mathmy_vec2_squared_lenght(v));
+
+    if (length > 0) {
+        return (Vec2){ v.x / length, v.y / length };
+    }
+}
+
+float mathmy_vec2_dot(Vec2 a, Vec2 b) {
+    return a.x * b.x + a.y * b.y;
+}
+
 float  mathmy_abs(float a) {
     if (a < 0) {
         return a * -1;
--- mathmy.h
+++ mathmy.h
@@ -8,6 +8,12 @@ typedef struct {
 } Vec2;
 
 Vec2 mathmy_vec2_add(Vec2 a, Vec2 b);
+Vec2 mathmy_vec2_sub(Vec2 a, Vec2 b);
+Vec2 mathmy_vec2_scale(Vec2 v, float scalar);
+Vec2 mathmy_vec2_norm(Vec2 v);
+float mathmy_vec2_squared_length(Vec2 v);
+float mathmy_vec2_dot(Vec2 a, Vec2 b);
 float  mathmy_abs(float a);
+
 
 #endif
